4297f7ece0d6ea91947093a77bd10adb46cc3319
doandv-java <61465374+doandv-java@users.noreply.github.com>
2020-03-05 15:28:27 +0700

    Add files via upload
    craete project

---
+++ src/main/java/doan/stores/utils/Constants.java
@@ -0,0 +1,14 @@
+package doan.stores.utils;
+
+import lombok.Data;
+
+@Data
+public class Constants {
+
+    public interface ADMIN {
+        String USER_NAME = "admin@gmail.com";
+        String PASS_WORD = "12345678";
+        String ROLE = "ROLE_ADMIN";
+        String NAME = "Admin";
+    }
+}
